4da02bcf93e28b692c589e0618ceed1a64938c8a
HeeketMehta <heeketmehta@gmail.com>
2020-04-03 00:44:45 +0530

    Cleaned Data

--- a.py
+++ a.py
@@ -23,147 +23,162 @@ df.drop(df.index[262], inplace=True)
 df.drop(df.index[433], inplace=True)
 df.drop(df.index[1914], inplace=True)
 
+df.drop(['STATION CODE'], axis=1, inplace = True)
 
 
+# print(df.columns)
+
+
+
+
+################################################	    DATA CLEANING AND MINING 		############################################################
 
 
 
 
 #################################################################################################################
 
+def clean_data_with_mean(df, attr):
+	list_of_all_attr = []
+	sum_avg = 0
+	c = 0
+	for i in df[attr]:
+		list_of_all_attr.append(i)
+		if i == 0.0 :
+			pass
+		else :
+			sum_avg = sum_avg + i
+			c = c + 1
+
+
+	mean_attr = sum_avg/c
+	# print("SUM OF ", attr, "is equal to ",sum_avg)
+
+	print("Mean OF ",attr," is  ",mean_attr)
+
+
+
+	# print(mean1)
+	for i in df[attr]:
+		if i == 0.0 :
+			df[attr].replace(0, round(mean_attr,2), inplace = True)
+		else:
+			pass
 
 
 
 
 
+def clean_data_with_median(df, attr):
+	list_of_all_attr = []
+	n = len(df[attr])
+	
+	median_index = int(n/2)
+	for i in df[attr]:
+		list_of_all_attr.append(i)
+
+	list_of_all_attr.sort()
+
+	# print(median_index)
+	median_value = list_of_all_attr[median_index]
+
+	print("MEDIAN OF ",attr," is  =  ",median_value)
+
+
+
+
+
+
+# 	for i in df[attr]:
+# 		if i == 0.0 :
+# 			df[attr].replace(0, round(mean_attr,2), inplace = True)
+# 		else:
+# 			pass
+
+
+
+# print(df.dtypes)
 
 
 ########################################################################### FOR CLEANING TEMPERATURE ####################################################
 
 
 
-list_of_all_temp = []
-sum_avg = 0
-c = 0
-for i in df['Temp']:
-	list_of_all_temp.append(i)
-	if i == 0 :
-		pass
-	else :
-		sum_avg = sum_avg + i
-		c = c + 1
+clean_data_with_mean(df, 'Temp')
+
+##########################################################################################################################################################
 
 
-mean_temp = sum_avg/c
 
-print("Mean Temp = ",mean_temp)
+###########################################################################    FOR CLEANING D.O. (mg/l)    ####################################################
+
+
+
+clean_data_with_mean(df, 'D.O. (mg/l)')
+
+
+##########################################################################################################################################################
+
+
+
+
+###########################################################################    FOR CLEANING PH    ####################################################
+
+
+clean_data_with_median(df, 'PH')
+
+
+##########################################################################################################################################################
+
+
+
+###########################################################################    FOR CLEANING CONDUCTIVITY (µmhos/cm)    ####################################################
+
+
+
+clean_data_with_median(df, 'CONDUCTIVITY (µmhos/cm)')
+
+
+##########################################################################################################################################################
+
+###########################################################################    FOR CLEANING NITRATENaN N+ NITRITENaNN (mg/l)    ####################################################
+
+
+
+clean_data_with_mean(df, 'NITRATENaN N+ NITRITENaNN (mg/l)')
+
+
+##########################################################################################################################################################
+
+###########################################################################    FOR CLEANING FECAL COLIFORM (MPN/100ml)    ####################################################
+
+
+
+clean_data_with_median(df, 'FECAL COLIFORM (MPN/100ml)')
+
+
+##########################################################################################################################################################
+
+###########################################################################    FOR CLEANING B.O.D. (mg/l)    ####################################################
+
+
+
+clean_data_with_mean(df, 'B.O.D. (mg/l)')
+
+
+##########################################################################################################################################################
+
+###########################################################################    FOR CLEANING TOTAL COLIFORM (MPN/100ml)Mean    ####################################################
+
+
+
+clean_data_with_median(df, 'TOTAL COLIFORM (MPN/100ml)Mean')
+
+
+##########################################################################################################################################################
 
 df.to_csv('ayushi.csv')
-# df['Temp'].replace(0, mean_temp)
-
-for i in df['Temp']:
-	if i == 0 :
-		df[i] = mean_temp
-	else:
-		pass
-
-########################### TO CALCULATE MSE FOR TEMP ############################
-list_mean = []
-
-list1 = list_of_all_temp
-n = len(list1)
-# print(n)
-for i in range(0,n):
-	list_mean.append(mean_temp)
-	
-
-
-MSE = np.square(np.subtract(list_mean,list_of_all_temp)).mean() 
-#################################################################################
 
 
 
-
-
-
-
-
-
-#################################################################################################
-
-
-################################################# FINDING MEAN AND NO OF NAN VALUES ###########################
-
-
-sum_avg = 0
-c = 0
-################################ NUMBER OF NAN VALUES FOR ALL ATTRIBUTES #########################################
-count_temp = 0  
-count_bod = 0 
-count_nit = 0
-count_do = 0
-count_ph = 0
-count_total = 0   
-count_cond = 0
-count_fecal = 0  
-
-
-for i in df['Temp']:
-	if i == 0 :
-		count_temp = count_temp + 1
-
-
-
-for i in df['D.O. (mg/l)']:
-	if i == 0 :
-		count_do = count_do + 1
-
-
-for i in df['PH']:
-	if i == 0 :
-		count_ph = count_ph + 1
-
-
-for i in df['CONDUCTIVITY (µmhos/cm)']:
-	if i == 0 :
-		count_cond = count_cond + 1
-
-
-
-for i in df['NITRATENaN N+ NITRITENaNN (mg/l)']:
-	if i == 0 :
-		count_nit = count_nit + 1
-
-
-
-for i in df['FECAL COLIFORM (MPN/100ml)']:
-	if i == 0 :
-		count_fecal = count_fecal + 1
-
-
-
-for i in df['TOTAL COLIFORM (MPN/100ml)Mean']:
-	if i == 0 :
-		count_total = count_total + 1
-
-
-
-for i in df['B.O.D. (mg/l)']:
-	if i == 0 :
-		count_bod = count_bod + 1
-
-
-
-print("TOTAL TEMP  NAN = ",count_temp)
-print("TOTAL DO  NAN = ",count_do)
-print("TOTAL BOD  NAN = ",count_bod)
-print("TOTAL PH  NAN = ",count_ph)
-print("TOTAL FECAL  NAN = ",count_fecal)
-print("TOTAL TOTAL NAN = ",count_total)
-print("TOTAL CONDUCTIVITY  NAN = ",count_cond)
-print("TOTAL NITRITE  NAN = ",count_nit)
-
-
-
-df.to_csv('shanay.csv')
-
+########################################################################################################################################################################

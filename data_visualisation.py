3ba4bc63a6aabf1553f88bd2ccb03ede091612f9
HeeketMehta <heeketmehta@gmail.com>
2020-04-13 16:35:30 +0530

    with bar graphs

--- data_visualisation.py
+++ data_visualisation.py
@@ -17,10 +17,58 @@ df.to_csv('state.csv')
 
 df2 = pd.read_csv('state.csv')
 
-for i in df['STATE']:
-	if i == 'GOA' or 'GUJARAT':
+
+
+
+def inital_plot(df, variable):
+	
+	data_0 = df[df['STATE'] == variable]
+	data_0.to_csv('state_Try.csv', mode = 'a', header = True)
+
+
+def following_plots(df, variable):
+	
+	data_0 = df[df['STATE'] == variable]
+	data_0.to_csv('state_Try.csv', mode = 'a', header = False)
+
+
+
+
+
+
+def count(var1):
+
+	df3 = pd.read_csv('state_Try.csv')
+	list_Try = []
+	for i in df3['STATE']:
+		list_Try.append(i)
+
+
+
+	return list_Try.count(var1)
+
+
+list1 = ['MEGHALAYA','GUJARAT','MAHARASHTRA','MANIPUR','KERALA','HARYANA', 'HIMACHAL PRADESH',
+		'RAJASTHAN','MEGHALAYA','MIZORAM','MANIPUR','PONDICHERRY']
+
+inital_plot(df2, 'GOA')
+for i in list1:
+	n = count(i)
+	if n>0:
 		pass
 	else:
-		df2.drop([i], axis = 1, inplace = True)
+		following_plots(df2, i)
 
-df2.to_csv('state1.csv')
+
+
+df3 = pd.read_csv('state_Try.csv')
+
+
+
+
+df3.groupby(
+  ['STATE','new_WQI']
+).size().unstack().plot(kind='bar',stacked=False,legend=True)
+
+plt.show()
+
